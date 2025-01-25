5ac4594b57f7b952cf7a2dc7c2e7ba80700b31d6
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-01-25 15:10:45 +0300

    Update Index.cshtml.cs

--- BaristaCoffee.Web/Pages/Index.cshtml.cs
+++ BaristaCoffee.Web/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 using BaristaCoffee.Dto.AboutDtos;
+using BaristaCoffee.Dto.ContactDtos;
 using BaristaCoffee.Dto.MenuDtos;
+using BaristaCoffee.Web.Services.Interfaces;
 using BaristaCoffee.Web.ViewModel;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
@@ -7,37 +9,47 @@ using Newtonsoft.Json;
 public class IndexModel : PageModel
 {
     private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IAboutService _aboutService;
+    private readonly IMenuService _menuService;
+    private readonly IBaristaService _baristaService;
+    private readonly ITestimonialService _testimonialService;
+    private readonly IContactService _contactService;
 
-    public IndexViewModel ViewModel { get; set; } = new IndexViewModel(); 
+    public IndexViewModel ViewModel { get; set; } = new IndexViewModel();
 
-    public IndexModel(IHttpClientFactory httpClientFactory)
+    public IndexModel(IHttpClientFactory httpClientFactory, IAboutService aboutService, IMenuService menuService, IBaristaService baristaService, ITestimonialService testimonialService, IContactService contactService)
     {
         _httpClientFactory = httpClientFactory;
+        _aboutService = aboutService;
+        _menuService = menuService;
+        _baristaService = baristaService;
+        _testimonialService = testimonialService;
+        _contactService = contactService;
     }
 
     public async Task OnGetAsync()
     {
         try
         {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessageAbout = await client.GetAsync("https://localhost:7066/api/About/GetAbout");
+            ViewModel.About = await _aboutService.GetAboutListAsync();
+            ViewModel.Menu = await _menuService.GetAllMenuAsync();
+            ViewModel.Barista = await _baristaService.GetAllBaristaAsync();
+            ViewModel.Testimonial = await _testimonialService.GetAllTestimonialsAsync();
 
-            if (responseMessageAbout.IsSuccessStatusCode)
-            {
-                var jsonDataAbout = await responseMessageAbout.Content.ReadAsStringAsync();
-                ViewModel.About = JsonConvert.DeserializeObject<List<GetAboutDto>>(jsonDataAbout);
-            }
-
-            var responseMessageMenu = await client.GetAsync("https://localhost:7066/api/Menu/GetAllMenu");
-            if (responseMessageMenu.IsSuccessStatusCode)
-            {
-                var jsonDataMenu = await responseMessageAbout.Content.ReadAsStringAsync();
-                ViewModel.Menu = JsonConvert.DeserializeObject<List<GetAllMenuDto>>(jsonDataMenu);
-            }
         }
         catch (Exception ex)
         {
             ModelState.AddModelError(string.Empty, "Veri alýnamadý: " + ex.Message);
         }
     }
+
+    public async Task OnPostAsync(CreateContactDto createContactDto)
+    {
+        await _contactService.CreateContact(createContactDto);
+        if (ModelState.IsValid)
+        {
+            ModelState.AddModelError(string.Empty, "Mesajýnýz baþarýyla gönderildi.");
+        }
+    }
+
 }
