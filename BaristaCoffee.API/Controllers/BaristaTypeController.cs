0446420d896895b5ec8c2994599bfaf152c8301d
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-01-26 19:09:08 +0300

    add baristaType api controller

---
+++ BaristaCoffee.API/Controllers/BaristaTypeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BaristaCoffee.API.Repositories.BaristaTypeRepositories;
+using BaristaCoffee.Dto.BaristaTypeDtos;
+
+
+namespace BaristaCoffee.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaristaTypeController : ControllerBase
+    {
+        private readonly IBaristaTypeRepository _baristaTypeRepository;
+
+        public BaristaTypeController(IBaristaTypeRepository baristaTypeRepository)
+        {
+            _baristaTypeRepository = baristaTypeRepository;
+        }
+
+        [HttpGet("GetAllBaristaTypes")]
+        public async Task<IActionResult> GetAllBaristaTypes()
+        {
+            var baristaTypes = await _baristaTypeRepository.GetAllBaristaTypeAsync();
+            return Ok(baristaTypes);
+        }
+
+        [HttpPost("CreateBaristaType")]
+        public async Task<IActionResult> CreateBaristaType(CreateBaristaTypeDto createBaristaTypeDto)
+        {
+            await _baristaTypeRepository.CreateBaristaTypeAsync(createBaristaTypeDto);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
