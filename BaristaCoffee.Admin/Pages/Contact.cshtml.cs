54ffe4a7dc3b08132726be5f05897f7da299e1f6
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-01-26 19:06:42 +0300

    update pages

--- BaristaCoffee.Admin/Pages/Contact.cshtml.cs
+++ BaristaCoffee.Admin/Pages/Contact.cshtml.cs
@@ -1,12 +1,23 @@
-using Microsoft.AspNetCore.Mvc;
+using BaristaCoffee.Admin.Services.Interfaces;
+using BaristaCoffee.Dto.ContactDtos;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BaristaCoffee.Admin.Pages
 {
     public class ContactModel : PageModel
     {
-        public void OnGet()
+        private readonly IContactService _contactService;
+
+        public required List<GetAllContactDto> Contacts { get; set; }
+
+        public ContactModel(IContactService contactService)
         {
+            _contactService = contactService;
+        }
+
+        public async Task OnGetAsync()
+        {
+           Contacts = await _contactService.GetAllContactAsync();
         }
     }
 }
