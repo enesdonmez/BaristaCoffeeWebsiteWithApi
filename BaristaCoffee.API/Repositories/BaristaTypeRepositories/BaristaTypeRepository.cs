df91f6913f9b3a72fe639ef2a404090dc31aa8bc
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-01-26 19:08:22 +0300

    add repository

---
+++ BaristaCoffee.API/Repositories/BaristaTypeRepositories/BaristaTypeRepository.cs
@@ -0,0 +1,50 @@
+﻿using BaristaCoffee.Dto.BaristaTypeDtos;
+using Dapper;
+using System.Data;
+
+namespace BaristaCoffee.API.Repositories.BaristaTypeRepositories
+{
+    public class BaristaTypeRepository(IDbConnection _dbConnection) : IBaristaTypeRepository
+    {
+        public async Task CreateBaristaTypeAsync(CreateBaristaTypeDto createBaristaTypeDto)
+        {
+            var sql = "INSERT INTO BaristaType(TypeName) VALUES(@TypeName)";
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("TypeName", createBaristaTypeDto.TypeName);
+            await _dbConnection.ExecuteAsync(sql, parameters);
+        }
+
+        public async Task DeleteBaristaTypeAsync(int id)
+        {
+            var sql = "DELETE FROM BaristaType WHERE Id = @Id";
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("Id", id);
+            await _dbConnection.ExecuteAsync(sql, parameters);
+        }
+
+        public async Task<List<GetAllBaristaTypeDto>> GetAllBaristaTypeAsync()
+        {
+             var sql = "SELECT * FROM BaristaType";
+            var types = await _dbConnection.QueryAsync<GetAllBaristaTypeDto>(sql);
+            return types.ToList();
+        }
+
+        public async Task<GetByIdBaristaTypeDto> GetBaristaTypeByIdAsync(int id)
+        {
+            var sql = "SELECT * FROM BaristaType WHERE Id = @Id";
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("Id", id);
+            var type = await _dbConnection.QueryFirstOrDefaultAsync<GetByIdBaristaTypeDto>(sql, parameters)!;
+            return type;
+        }
+
+        public async Task UpdateBaristaTypeAsync(UpdateBaristaTypeDto updateBaristaTypeDto)
+        {
+            var sql = "UPDATE BaristaType SET TypeName = @TypeName WHERE Id = @Id";
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("Id", updateBaristaTypeDto.Id);
+            parameters.Add("TypeName", updateBaristaTypeDto.TypeName);
+            await _dbConnection.ExecuteAsync(sql, parameters);
+        }
+    }
+}
---
+++ BaristaCoffee.API/Repositories/BaristaTypeRepositories/IBaristaTypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using BaristaCoffee.Dto.BaristaTypeDtos;
+
+namespace BaristaCoffee.API.Repositories.BaristaTypeRepositories
+{
+    public interface IBaristaTypeRepository
+    {
+        Task<List<GetAllBaristaTypeDto>> GetAllBaristaTypeAsync();
+
+        Task<GetByIdBaristaTypeDto> GetBaristaTypeByIdAsync(int id);
+
+        Task CreateBaristaTypeAsync(CreateBaristaTypeDto createBaristaTypeDto);
+
+        Task UpdateBaristaTypeAsync(UpdateBaristaTypeDto updateBaristaTypeDto);
+
+        Task DeleteBaristaTypeAsync(int id);
+    }
+}
