afd84cfe2f11a20a418b5424ebb9af6063e894ea
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-01-30 18:14:43 +0300

    update service

---
+++ BaristaCoffee.Admin/Services/Concretes/MenuCategoryService.cs
@@ -0,0 +1,43 @@
+﻿using BaristaCoffee.Admin.Services.Interfaces;
+using BaristaCoffee.Dto.ContactDtos;
+using BaristaCoffee.Dto.MenuCategoryDtos;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+
+namespace BaristaCoffee.Admin.Services.Concretes
+{
+    public class MenuCategoryService : IMenuCategoryService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MenuCategoryService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task CreateMenuCategoryAsync(CreateMenuCategoryDto createMenuCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.PostAsJsonAsync("https://localhost:7066/api/MenuCategory/CreateMenuCategory", createMenuCategoryDto);
+            if (response.StatusCode == HttpStatusCode.Created)
+            {
+
+            }
+        }
+
+        public async Task<List<GetAllMenuCategoryDto>> GetAllMenuCategoryAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/MenuCategory/GetAllMenuCategory");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<GetAllMenuCategoryDto>>(jsonData)!;
+            }
+
+            return new List<GetAllMenuCategoryDto>();
+        }
+    }
+}
---
+++ BaristaCoffee.Admin/Services/Interfaces/IMenuCategoryService.cs
@@ -0,0 +1,11 @@
+﻿using BaristaCoffee.Dto.MenuCategoryDtos;
+
+namespace BaristaCoffee.Admin.Services.Interfaces
+{
+    public interface IMenuCategoryService
+    {
+        Task<List<GetAllMenuCategoryDto>> GetAllMenuCategoryAsync();
+
+        Task CreateMenuCategoryAsync(CreateMenuCategoryDto createMenuCategoryDto);
+    }
+}
