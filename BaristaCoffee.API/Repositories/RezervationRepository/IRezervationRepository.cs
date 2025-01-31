4175e85a40993468b9cdff08ac54ccbd5ff73201
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-01-31 18:17:36 +0300

    add rezervation repositories

---
+++ BaristaCoffee.API/Repositories/RezervationRepository/IRezervationRepository.cs
@@ -0,0 +1,13 @@
+﻿using BaristaCoffee.Dto.RezervationDtos;
+
+namespace BaristaCoffee.API.Repositories.RezervationRepository
+{
+    public interface IRezervationRepository
+    {
+        Task<List<GetAllRezervationDto>> GetAllRezervationAsync();
+        Task<GetByIdRezervationDto> GetRezervationByIdAsync(int id);
+        Task CreateRezervationAsync(CreateRezervationDto createRezervationDto);
+        Task UpdateRezervationAsync(UpdateRezervationDto updateRezervationDto);
+        Task DeleteRezervationAsync(int id);
+    }
+}
---
+++ BaristaCoffee.API/Repositories/RezervationRepository/RezervationRepository.cs
@@ -0,0 +1,53 @@
+﻿using BaristaCoffee.Dto.RezervationDtos;
+using Dapper;
+using System.Data;
+
+namespace BaristaCoffee.API.Repositories.RezervationRepository
+{
+    public class RezervationRepository : IRezervationRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public RezervationRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task CreateRezervationAsync(CreateRezervationDto createRezervationDto)
+        {
+            var sql = "SP_INSERT_REZERVATION";
+            DynamicParameters parameters = new();
+            parameters.Add("@fullName", createRezervationDto.FullName);
+            parameters.Add("@phone", createRezervationDto.Phone);
+            parameters.Add("@rezervationTime", createRezervationDto.RezervationTime);
+            parameters.Add("@rezervationDate", createRezervationDto.RezervationDate,DbType.Date);
+            parameters.Add("@numberOfPeople", createRezervationDto.NumberOfPeople);
+            parameters.Add("@comment", createRezervationDto.Comment);
+            parameters.Add("@isActive", createRezervationDto.IsActive);
+
+            await _connection.ExecuteAsync(sql, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public Task DeleteRezervationAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GetAllRezervationDto>> GetAllRezervationAsync()
+        {
+            string sql = "SP_SELECT_REZERVATION";
+            var result = await _connection.QueryAsync<GetAllRezervationDto>(sql,CommandType.StoredProcedure);
+            return result.ToList();
+        }
+
+        public Task<GetByIdRezervationDto> GetRezervationByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateRezervationAsync(UpdateRezervationDto updateRezervationDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
