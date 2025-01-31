f3653dde078e83c79d992a6ea9e0bb36c4d5b7b2
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-01-31 18:17:12 +0300

    add new dto

---
+++ BaristaCoffee.Dto/RezervationDtos/CreateRezervationDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaristaCoffee.Dto.RezervationDtos
+{
+    public class CreateRezervationDto
+    {
+
+        public string? FullName { get; set; }
+
+        public string? Phone { get; set; }
+
+        public TimeSpan RezervationTime { get; set; }
+
+        public DateOnly RezervationDate { get; set; }
+
+        public Int16 NumberOfPeople { get; set; }
+
+        public string Comment { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; }
+
+
+    }
+}
---
+++ BaristaCoffee.Dto/RezervationDtos/UpdateRezervationDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaristaCoffee.Dto.RezervationDtos
+{
+    public class UpdateRezervationDto
+    {
+        public int Id { get; set; }
+
+        public string? FullName { get; set; }
+
+        public string? Phone { get; set; }
+
+        public TimeSpan RezervationTime { get; set; }
+
+        public DateOnly RezervationDate { get; set; }
+
+        public Int16 NumberOfPeople { get; set; }
+
+        public string? Comment { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
